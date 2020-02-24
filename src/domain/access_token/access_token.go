1c581263f1c3a6b00f74a24fc92001ee80e3e124
Pastukhov A <pactx@yandex.ru>
2020-02-24 18:08:54 +0300

    init

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -3,7 +3,7 @@ package access_token
 import (
 	"fmt"
 	"github.com/pastukhov-aleksandr/bookstore_aouth_api/src/utils/crypto_utils"
-	"github.com/pastukhov-aleksandr/bookstore_aouth_api/src/utils/errors"
+	"github.com/pastukhov-aleksandr/bookstore_utils-go/rest_errors"
 	"strings"
 	"time"
 )
@@ -27,7 +27,7 @@ type AccessTokenRequest struct {
 	ClientSecret string `json:"client_secret"`
 }
 
-func (at *AccessTokenRequest) Validate() *errors.RestErr {
+func (at *AccessTokenRequest) Validate() rest_errors.RestErr {
 	switch at.GrantType {
 	case grantTypePassword:
 		break
@@ -36,7 +36,7 @@ func (at *AccessTokenRequest) Validate() *errors.RestErr {
 		break
 
 	default:
-		return errors.NewBadRequestError("invalid grant_type parameter")
+		return rest_errors.NewBadRequestError("invalid grant_type parameter")
 	}
 
 	//TODO: Validate parameters for each grant_type
@@ -50,19 +50,19 @@ type AccessToken struct {
 	Expires     int64  `json:"expires"`
 }
 
-func (at *AccessToken) Validate() *errors.RestErr {
+func (at *AccessToken) Validate() rest_errors.RestErr {
 	at.AccessToken = strings.TrimSpace(at.AccessToken)
 	if at.AccessToken == "" {
-		return errors.NewBadRequestError("invalid access token id")
+		return rest_errors.NewBadRequestError("invalid access token id")
 	}
 	if at.UserId <= 0 {
-		return errors.NewBadRequestError("invalid user id")
+		return rest_errors.NewBadRequestError("invalid user id")
 	}
 	if at.ClientId <= 0 {
-		return errors.NewBadRequestError("invalid client id")
+		return rest_errors.NewBadRequestError("invalid client id")
 	}
 	if at.Expires <= 0 {
-		return errors.NewBadRequestError("invalid expiration time")
+		return rest_errors.NewBadRequestError("invalid expiration time")
 	}
 	return nil
 }
@@ -75,7 +75,7 @@ func GetNewAccessToken(userId int64) AccessToken {
 }
 
 func (at AccessToken) IsExpired() bool {
-	return time.Now().UTC().After(time.Unix(at.Expires, 0))
+	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
 }
 
 func (at *AccessToken) Generate() {
